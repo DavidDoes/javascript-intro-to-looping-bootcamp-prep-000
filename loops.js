11c7b2148b65f7c126bb9c51e5dfb34afccc4eb2
David Clark <dlc.clark@gmail.com>
2018-01-16 01:08:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,9 @@ function forLoop (arr){
 }
 
 for (let i = 0; i < 25; i++){
-  if (i <= 1){
-      array.push("I am ${i} strange loop.");
+  if (i === 1){
+    array.push("I am ${i} strange loop.");
+  } else {
+    array.push("I am ${i} strange loops.")
   }
 }
